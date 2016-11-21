23d7a88c10b03bcdce1db15d29a720dbd8e578d9
Hunter <hunter205@iis.sinica.edu.tw>
2016-11-21 16:27:57 +0800

    Add the very first main program.

---
+++ DiReCT/DSModule.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+
+namespace DiReCT
+{
+    class DSModule
+    {
+        // State control variable
+        static bool IsInitialized = false;
+        static bool IsReady = false;
+        static bool IsContinue = true;
+
+        public static void DSInit(object objectParameters)
+        {
+            ThreadParameters threadParameters = (ThreadParameters)objectParameters;
+
+            // Event variables
+            WaitHandle[] initializationEvents = new WaitHandle[(int)EventIndex.NumberOfWorkEvents];
+            int indexOfSignalEvent;
+
+            if (IsInitialized == true)
+            {
+                Debug.WriteLine("DSInit initial twice.");
+                return;
+            }
+
+            // Variables initialization
+            IsInitialized = false;
+            IsReady = false;
+            IsContinue = true;
+
+            // Event variables initialization
+            initializationEvents[(int)EventIndex.StartWorkEvent] = threadParameters.StartWorkEvent;
+            initializationEvents[(int)EventIndex.TerminateWorkEvent] = threadParameters.TerminateWorkEvent;
+
+            //
+            // Modules initialization code here...
+            //
+
+            //
+            // End of Phase 1
+            //
+            threadParameters.ReadyToWorkEvent.Set();
+            Debug.WriteLine("DSInit complete Phase 1 Initialization");
+
+            indexOfSignalEvent = WaitHandle.WaitAny(initializationEvents);
+
+            if (indexOfSignalEvent != (int)EventIndex.StartWorkEvent)
+                goto Return;
+
+            IsInitialized = true;
+            Debug.WriteLine("DSInit complete Phase 2 Initialization and start work.");
+
+            //
+            // Main Thread of DS module (begin)
+            //
+            while (IsContinue == true)
+            {
+                IsReady = true;
+
+                // temporary demo code
+                initializationEvents[(int)EventIndex.TerminateWorkEvent].WaitOne();
+
+                //
+                // Wait for working events
+                // Switch case for different events
+                //
+            }
+
+            Return:
+            //
+            // Cleanup code
+            //
+            return;
+        }
+    }
+}
---
+++ DiReCT/DiReCTCore/MainProgram.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Diagnostics;
+
+namespace DiReCT.DiReCTCore
+{
+    class DiReCTMainProgram : Application
+    {
+        /// <summary>
+        /// The main entry point for the application
+        /// </summary>
+        /// 
+
+        private static Thread[] moduleThreadHandles 
+            = new Thread[(int)ThreadRequiredModule.NumberOfModules];
+        private static Thread UIThreadHandle;
+        private static ModuleControlData[] modulesControlData 
+            = new ModuleControlData[(int)ThreadRequiredModule.NumberOfModules];
+        private static AutoResetEvent[] moduleReadyEvents 
+            = new AutoResetEvent[(int)ThreadRequiredModule.NumberOfModules];
+        private static bool 
+            notInitializationTimeout = true; // Whether initialization processes
+                                             // were completed in time
+
+        [MTAThread]
+        static void Main()
+        {
+            // Initialize thread objects of modules
+            moduleThreadHandles[(int)ThreadRequiredModule.AAA] 
+                = new Thread(AAAModule.AAAInit);
+            moduleThreadHandles[(int)ThreadRequiredModule.DM] 
+                = new Thread(DMModule.DMInit);
+            moduleThreadHandles[(int)ThreadRequiredModule.DS]
+                = new Thread(DSModule.DSInit);
+            moduleThreadHandles[(int)ThreadRequiredModule.MAN]
+                = new Thread(MANModule.MANInit);
+            moduleThreadHandles[(int)ThreadRequiredModule.RTQC]
+                = new Thread(RTQCModule.RTQCInit);
+
+            // Initialize the thread object of UI thread
+            UIThreadHandle = new Thread(UIMainFunction);
+            UIThreadHandle.SetApartmentState(ApartmentState.STA);
+
+            // Initialize control data of modules
+            for (int i = 0; i < (int)ThreadRequiredModule.NumberOfModules; i++)
+            {
+                modulesControlData[i] = new ModuleControlData();
+            }
+
+            // Start to execute modules
+            try
+            {
+                moduleThreadHandles[(int)ThreadRequiredModule.AAA]
+                    .Start(modulesControlData[(int)ThreadRequiredModule.AAA]
+                    .ThreadParameters);
+                moduleThreadHandles[(int)ThreadRequiredModule.DM]
+                    .Start(modulesControlData[(int)ThreadRequiredModule.DM]
+                    .ThreadParameters);
+                moduleThreadHandles[(int)ThreadRequiredModule.DS]
+                    .Start(modulesControlData[(int)ThreadRequiredModule.DS]
+                    .ThreadParameters);
+                moduleThreadHandles[(int)ThreadRequiredModule.MAN]
+                    .Start(modulesControlData[(int)ThreadRequiredModule.MAN]
+                    .ThreadParameters);
+                moduleThreadHandles[(int)ThreadRequiredModule.RTQC]
+                    .Start(modulesControlData[(int)ThreadRequiredModule.RTQC]
+                    .ThreadParameters);
+            }
+            catch (OutOfMemoryException e)
+            {
+                Debug.WriteLine(e.Message);
+                Debug.WriteLine("Some module thread cannot start.");
+                goto InitializationFail;
+            }
+
+            // Set up the array of Ready events
+            for (int i = 0; i < (int)ThreadRequiredModule.NumberOfModules; i++)
+            {
+                moduleReadyEvents[i] = modulesControlData[i].ThreadParameters
+                                                            .ReadyToWorkEvent;
+            }
+
+            Timer timerOfIntialization 
+                = new Timer(new TimerCallback(InitializationTimeOutEventHandler),
+                            null,
+                            (int)TimeInterval.LongTime,
+                            Timeout.Infinite);
+
+            while (notInitializationTimeout)
+            {
+                if (WaitHandle.WaitAll(moduleReadyEvents, 
+                                       (int)TimeInterval.VeryShortTime, 
+                                       true))
+                {
+                    Debug.WriteLine(
+                        "Phase 1 initialization of all modules complete!");
+                    break;
+                }
+                else
+                {
+                    for (int i = 0; i < moduleThreadHandles.Length; i++)
+                    {
+                        Thread moduleThreadHandle = moduleThreadHandles[i];
+                        if (!moduleThreadHandle.IsAlive)
+                        {
+                            Debug.WriteLine("Phase 1 initialization of "
+                                            + i + "-th module fails!");
+                            notInitializationTimeout = false;
+                            goto InitializationFail;
+                        }
+                    }
+                }
+            }
+
+            timerOfIntialization.Dispose();
+
+            foreach (ModuleControlData moduleControlData in modulesControlData)
+            {
+                moduleControlData.ThreadParameters.StartWorkEvent.Set();
+            }
+
+            try
+            {
+                UIThreadHandle.Start();
+            }
+            catch (OutOfMemoryException e)
+            {
+                Debug.WriteLine(e.Message);
+                Debug.WriteLine("UI thread can not start!");
+                goto InitializationFail;
+            }
+
+            //
+            // Code of Helper thread here...
+            //
+
+            UIThreadHandle.Join();
+
+            Return:
+            // singal all created thread to prepare to terminate
+            foreach (ModuleControlData moduleControlData in modulesControlData)
+            {
+                moduleControlData.ThreadParameters.TerminateWorkEvent.Set();
+            }
+
+            // wait for all created threads to be terminated
+            foreach (Thread moduleThreadHandle in moduleThreadHandles)
+            {
+                if (moduleThreadHandle.ThreadState 
+                    != System.Threading.ThreadState.Unstarted)
+                {
+                    moduleThreadHandle.Join();
+                }
+            }
+            return;
+
+            InitializationFail:
+                goto Return;
+        }
+        
+        private static void UIMainFunction()
+        {
+            DiReCTMainProgram DiReCTmainProgram = new DiReCTMainProgram();
+            DiReCTmainProgram.StartupUri 
+                = new System.Uri("DiReCTCore\\MainWindow.xaml", 
+                                 System.UriKind.Relative);
+            DiReCTmainProgram.Run();
+        }
+
+        private static void InitializationTimeOutEventHandler(object state)
+        {
+            notInitializationTimeout = false;
+        }
+    }    
+}
