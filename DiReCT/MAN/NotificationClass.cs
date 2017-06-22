381d42e88f289783d32ef4cf99669d697606a3fe
hunter205git <hunter205@iis.sinica.edu.tw>
2017-06-22 17:14:13 +0800

    Add the header of the file.

--- DiReCT/MAN/NotificationClass.cs
+++ DiReCT/MAN/NotificationClass.cs
@@ -1,4 +1,45 @@
-﻿using System;
+﻿/*
+ * Copyright (c) 2016 Academia Sinica, Institude of Information Science
+ * 
+ *  This file is part of DiReCT.
+ *
+ *  DiReCT is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Foobar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Project Name:
+ * 
+ *      DiReCT(Disaster Record Capture Tool)
+ * 
+ * File Description:
+ * File Name:
+ * 
+ *      NotificationClass.cs
+ * 
+ * Abstract:
+ *      
+ *      This file contains classes for monitoring time and device location, and
+ *      pushing a notification. The notification class contains all information
+ *      a notification needs. The notification manager manages notifications,
+ *      such as push and cancel a notification.
+ *
+ * Authors:
+ * 
+ *      Hunter Hsieh, hunter205@iis.sinica.edu.tw  
+ *      Jeff Chen, jeff@iis.sinica.edu.tw
+ * 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Windows.Threading;
