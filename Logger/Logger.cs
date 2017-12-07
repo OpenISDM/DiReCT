fa4803763ecfdf0dc71363937a477171d3b67508
唐振嚴 <kenneth@nssh.ntpc.edu.tw>
2017-12-07 23:34:32 +0800

    Logger library

---
+++ Logger/Logger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace Logger
+{
+    public enum Log
+    {
+        GeneralEvent = 0,
+        ErrorEvent
+    }
+
+    public static class Logger
+    {
+        private static string LogsPath = AppDomain.CurrentDomain.BaseDirectory
+            + "\\Logs\\";
+        private static object GeneralEventLogLock = new object();
+        private static object ErrorEventLock = new object();
+
+        /// <summary>
+        /// Write message to log file.
+        /// </summary>
+        /// <param name="log"></param>
+        /// <param name="Message"></param>
+        public static void Write(this Log log, string Message)
+        {
+            if (log == Log.GeneralEvent)
+            {
+                //Prevent multiple threads simultaneously writing
+                lock (GeneralEventLogLock)
+                {
+                    //Check if the folder exists
+                    if (!Directory.Exists(LogsPath))
+                        Directory.CreateDirectory(LogsPath);
+
+                    File.AppendAllText(LogsPath + "GeneralLog.txt",
+                        DateTime.Now.ToString() + ": " + Message + "\r\n");
+                }
+            }
+            else if (log == Log.ErrorEvent)
+            {
+                lock (ErrorEventLock)
+                {
+                    if (!Directory.Exists(LogsPath))
+                        Directory.CreateDirectory(LogsPath);
+
+                    File.AppendAllText(LogsPath + "ErrorLog.txt",
+                        DateTime.Now.ToString() + ": " + Message + "\r\n");
+                }
+            }
+        }
+    }
+}
