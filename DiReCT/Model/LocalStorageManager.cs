b93ef0c763bf7f3bfd616d07f09406d3269bb4d1
唐振嚴 <kenneth@gm.nssh.ntpc.edu.tw>
2018-01-25 00:03:34 +0800

    Add local storage comment

--- DiReCT/Model/LocalStorageManager.cs
+++ DiReCT/Model/LocalStorageManager.cs
@@ -1,4 +1,35 @@
-﻿using System;
+﻿/*
+ * Copyright (c) 2016 Academia Sinica, Institude of Information Science
+ *
+ * License:
+ *      GPL 3.0 : The content of this file is subject to the terms and 
+ *      conditions defined in file 'COPYING.txt', which is part of this source
+ *      code package.
+ *
+ * Project Name:
+ * 
+ *      DiReCT(Disaster Record Capture Tool)
+ * 
+ * File Description:
+ * File Name:
+ * 
+ *      LocalStorageManager.cs
+ * 
+ * Abstract:
+ *      
+ *      Local Storage Manager provides functions for DM and DS 
+ *      to access the event data, user data and record data 
+ *      in the local storage.
+ *
+ * Authors:
+ * 
+ *      Hunter Hsieh, hunter205@iis.sinica.edu.tw  
+ *      Jeff Chen, jeff@iis.sinica.edu.tw
+ *      Kenneth Tang, kenneth@gm.nssh.ntpc.edu.tw
+ * 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
@@ -35,17 +66,17 @@ namespace DiReCT.Model
 
             try
             {
-                //Check if the source exists
+                // Check if the source exists
                 if (File.Exists(SourcePath))
                 {
-                    //Obtain file Extension
+                    // Obtain file Extension
                     string Extension = "." +
-                        System.IO.Path.GetExtension(SourcePath);
-                    //Generate a new Id ID file name
+                        Path.GetExtension(SourcePath);
+                    // Generate a new Id ID file name
                     FileName = Guid.NewGuid().ToString() + Extension;
                     string DestPath = TemporaryPath + FileName;
 
-                    //Check destination folder exists and copy
+                    // Check destination folder exists and copy
                     if (!File.Exists(DestPath))
                     {
                         File.Copy(SourcePath, DestPath);
@@ -65,6 +96,8 @@ namespace DiReCT.Model
 
     public class RecordStorage
     {
+        // This folder, used to store the record of each recorder
+        // There will be one file for each recorder
         private static string RecordPath = 
             LocalStorage.StoragePath + @"Record\";
 
@@ -73,16 +106,26 @@ namespace DiReCT.Model
         static ManualResetEvent ModuleAbortEvent, ModuleStartWorkEvent;
         static AutoResetEvent ModuleReadyEvent;
 
+        // Multi-threaded environment, used to lock resources
         private static object RecordWorkQueueLock = new object();
         private static object FileLock = new object();
         private static object dictionaryLock = new object();
 
+        // Add record work, Update record work, Remove record work
         private static Queue<(Action<(dynamic, RecordStorageLocation)>,
             (dynamic,RecordStorageLocation))> RecordWorkQueue;
+
+        // Record storage loads the corresponding defective data or clean data 
+        // according to the recorder's current data.
         private static Guid CurrentRecorder;
+        // Tkey is record class full name, Tvalue is records
         private static Dictionary<string, List<dynamic>> defectiveData;
         private static Dictionary<string, List<dynamic>> cleanData;
 
+        /// <summary>
+        /// Record storage module initialization
+        /// </summary>
+        /// <param name="objectParameters"></param>
         public static void RSInit(object objectParameters)
         {
             moduleControlDataBlock
@@ -123,13 +166,24 @@ namespace DiReCT.Model
             CleanupExit();
         }
 
+        /// <summary>
+        /// Obtain the recorder's data
+        /// </summary>
+        /// <param name="Records">Input record id</param>
+        /// <returns>
+        /// First dynamic array is defective Data,
+        /// second dynamic array is cleanData
+        /// </returns>
         public (dynamic[], dynamic[]) GetRecord(Guid Records)
         {
+            // Check if the currently loaded logger is the desired logger
+            // if true thhen return defective data and clean data
             if (Records == CurrentRecorder)
                 return (RecordConsolidation(defectiveData),
                     RecordConsolidation(cleanData));
 
-
+            // Read the data of a specific recorder
+            // from the hard disk and send it back
             LoadOnFile(Records, 
                 out Dictionary<string, List<dynamic>> defectiveRecord, 
                 out Dictionary<string, List<dynamic>> cleanRecord);
@@ -137,45 +191,83 @@ namespace DiReCT.Model
                     RecordConsolidation(cleanRecord));
         }
 
+        /// <summary>
+        /// Add record to local storage
+        /// </summary>
+        /// <param name="Record"></param>
+        /// <param name="RecordLocation">
+        /// defective data or clen data
+        /// </param>
         public void Add(dynamic Record, RecordStorageLocation RecordLocation)
         {
+            // Enqueue add record work to work queue
             lock (RecordWorkQueueLock)
                 RecordWorkQueue.Enqueue((AddRecordWork,
                     (Record, RecordLocation)));
         }
 
+        /// <summary>
+        /// Update record to local storage
+        /// </summary>
+        /// <param name="Record"></param>
+        /// <param name="RecordLocation">
+        /// defective data or clen data
+        /// </param>
         public void Update(dynamic Record,RecordStorageLocation RecordLocation)
         {
+            // Enqueue update record work to work queue
             lock (RecordWorkQueueLock)
                 RecordWorkQueue.Enqueue((UpdateRecordWork,
                     (Record, RecordLocation)));
         }
 
+        /// <summary>
+        /// Remove record to local storage
+        /// </summary>
+        /// <param name="Record"></param>
+        /// <param name="RecordLocation">
+        /// defective data or clen data
+        /// </param>
         public void Remove(dynamic Record, 
             RecordStorageLocation RecordLocation)
         {
+            // Enqueue remove record work to work queue
             lock (RecordWorkQueueLock)
                 RecordWorkQueue.Enqueue((RemoveRecordWork,
                     (Record, RecordLocation)));
         }
 
+        /// <summary>
+        /// Function to handle work (add, update, remove)
+        /// </summary>
         private static void RecordStoraageWork()
         {
-            while(!ModuleAbortEvent
+            // Check ModuleAbortEvent periodically
+            while (!ModuleAbortEvent
                         .WaitOne((int)TimeInterval.VeryVeryShortTime))
             {
+                // Wait module abort event or work
                 SpinWait.SpinUntil(() => (ModuleAbortEvent
                         .WaitOne((int)TimeInterval.VeryVeryShortTime) && 
                         RecordWorkQueue.Count != 0));
 
-                if (RecordWorkQueue.Count != 0)
-                {
-                    var Work = RecordWorkQueue.Dequeue();
-                    Work.Item1.Invoke(Work.Item2);
-                }
+                // Check whether inside the work queue has job or not
+                lock (RecordWorkQueueLock)
+                    if (RecordWorkQueue.Count != 0)
+                    {
+                        var Work = RecordWorkQueue.Dequeue();
+                        Work.Item1.Invoke(Work.Item2);
+                    }
             }
         }
 
+        /// <summary>
+        /// load json data on file then convert Json data into record objects
+        /// </summary>
+        /// <param name="Recorder">record id</param>
+        /// <param name="defectiveData"></param>
+        /// <param name="cleanData"></param>
+        /// <returns></returns>
         private static bool LoadOnFile(
             Guid Recorder,
             out Dictionary<string, List<dynamic>> defectiveData,
@@ -188,7 +280,7 @@ namespace DiReCT.Model
 
             try
             {
-                //Open file and get json data
+                // Open file and load json data
                 lock (FileLock)
                     using (StreamReader streamReader
                         = new StreamReader(RecordPath + Recorder.ToString(),
@@ -198,7 +290,7 @@ namespace DiReCT.Model
                         JSON = JsonConvert.DeserializeObject(buffer);
                     }
 
-                //Convert Json data into record objects
+                // Convert Json data into record objects
                 lock (dictionaryLock)
                 {
                     if (!ConvertToRecord((string)JSON.Defective,
@@ -219,10 +311,20 @@ namespace DiReCT.Model
             return LoadingSuccess;
         }
 
+        /// <summary>
+        /// work function for add record
+        /// </summary>
+        /// <param name="Record">
+        /// Item1 is record
+        /// Item2 is set the purpose of storage (defective data, clean data)
+        /// </param>
         private static void AddRecordWork(
             (dynamic, RecordStorageLocation) Record)
         {
-            lock(dictionaryLock)
+            // Check if the currently loaded logger is the desired logger
+            // if true thhen adding
+            // else reload data before adding
+            lock (dictionaryLock)
                 if (CurrentRecorder == Record.Item1.Recorder)
                     AddRecord(Record);
                 else
@@ -239,9 +341,19 @@ namespace DiReCT.Model
             SaveChange();
         }
 
+        /// <summary>
+        /// work function for update record
+        /// </summary>
+        /// <param name="Record">
+        /// Item1 is record
+        /// Item2 is set the purpose of storage (defective data, clean data)
+        /// </param>
         private static void UpdateRecordWork(
             (dynamic, RecordStorageLocation) Record)
         {
+            // Check if the currently loaded logger is the desired logger
+            // if true thhen updating
+            // else reload data before updating
             lock (dictionaryLock)
                 if (CurrentRecorder == Record.Item1.Recorder)
                     UpdateRecord(Record);
@@ -259,10 +371,20 @@ namespace DiReCT.Model
             SaveChange();
         }
 
+        /// <summary>
+        /// work function for remove record
+        /// </summary>
+        /// <param name="Record">
+        /// Item1 is record
+        /// Item2 is set the purpose of storage (defective data, clean data)
+        /// </param>
         private static void RemoveRecordWork(
             (dynamic, RecordStorageLocation) Record)
         {
-            lock(dictionaryLock)
+            // Check if the currently loaded logger is the desired logger
+            // if true thhen removing
+            // else reload data before removing
+            lock (dictionaryLock)
                 if (CurrentRecorder == Record.Item1.Recorder)
                     RemoveRecord(Record);
                 else
@@ -279,12 +401,20 @@ namespace DiReCT.Model
             SaveChange();
         }
 
+        /// <summary>
+        /// Add record function
+        /// </summary>
+        /// <param name="Record"></param>
         private static void AddRecord((dynamic, RecordStorageLocation) Record)
         {
+            // Tkey of dictionary (defectiveData, cleanData)
             Type RecordType = Record.Item1.GetType();
             switch (Record.Item2)
             {
                 case RecordStorageLocation.DefectiveData:
+                    // Check if Tkey exists
+                    // if exists then adding
+                    // else add Tkey before adding new value
                     if (!defectiveData.ContainsKey(RecordType.FullName))
                         defectiveData.Add
                             (RecordType.FullName, new List<dynamic>());
@@ -302,9 +432,17 @@ namespace DiReCT.Model
             }
         }
 
+        /// <summary>
+        /// Update record function
+        /// </summary>
+        /// <param name="Record"></param>
         private static void UpdateRecord(
             (dynamic, RecordStorageLocation) Record)
         {
+            // This function finds the key which stores record,  
+            // and get the record list.
+            // The the record with the same record id will be removed. 
+            // The new record will added.
             var RecordId = Record.Item1.Id;
             Type RecordType = Record.Item1.GetType();
             List<dynamic> Records;
@@ -334,6 +472,10 @@ namespace DiReCT.Model
             }
         }
 
+        /// <summary>
+        /// Remove record function
+        /// </summary>
+        /// <param name="Record"></param>
         private static void RemoveRecord(
             (dynamic, RecordStorageLocation) Record)
         {
@@ -358,15 +500,22 @@ namespace DiReCT.Model
         private static bool SaveChange()
         {
             string Json;
+            // Convert record to json
             lock (dictionaryLock)
                 Json = JsonConvert.SerializeObject(new {
                     DefectiveData = JsonConvert.SerializeObject(defectiveData),
                     CleanData = JsonConvert.SerializeObject(cleanData)
                 });
 
+            // save json to file 
             return SaveJsonToFile(CurrentRecorder,Json);
         }
 
+        /// <summary>
+        /// Consolidate multiple lists into an array
+        /// </summary>
+        /// <param name="Records"></param>
+        /// <returns></returns>
         private static dynamic[] RecordConsolidation(
             Dictionary<string, List<dynamic>> Records)
         {
@@ -390,11 +539,12 @@ namespace DiReCT.Model
             {
                 lock (FileLock)
                 {
-                    //Check the folder exists
+                    // Check the folder exists
                     if (!Directory.Exists(RecordPath))
                         Directory.CreateDirectory(RecordPath);
 
-                    //Write data to the file
+                    // Write data to the file
+                    // Named using Logger Id
                     using (StreamWriter streamWriter
                         = new StreamWriter(RecordPath + Recorder.ToString()
                         , false))
@@ -430,8 +580,10 @@ namespace DiReCT.Model
             {
                 JObject JsonObject = JObject.Parse(DataCategory);
 
+                // According to the number of record type
                 foreach (KeyValuePair<string,JToken> KVP in JsonObject)
                 {
+                    // json reverse into objects, for example: list <flood>
                     Type DisasterType = Type.GetType(KVP.Key);
                     Type DisasterListType = typeof(List<>)
                         .MakeGenericType(new Type[] { DisasterType });
@@ -442,6 +594,7 @@ namespace DiReCT.Model
                     List<dynamic> RecordList = new List<dynamic>();
                     RecordList.AddRange((IEnumerable<dynamic>)Records);
 
+                    // Add record list to the dictionary
                     OutPutData.Add(DisasterType.FullName, RecordList);
                 }
                 IsConvertSuccess = true;
