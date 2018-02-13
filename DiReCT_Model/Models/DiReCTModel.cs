dc5dcdea38deee5d0f04f0732ee4e4170731bf40
唐振嚴 <Kenneth@gm.nssh.ntpc.edu.tw>
2018-02-13 16:35:26 +0800

    DiReCT model prototype

---
+++ DiReCT_Model/Models/DiReCTModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+// Through Microsoft Entity Framework, 
+// these class contains the functions which could insert the records
+// to the corresponding table in the database
+// The objects in the class would be set as the field name in the table
+namespace DiReCT.Models
+{
+    public partial class DisasterInformation
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string DisasterName { get; set; }
+
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreateOn { get; set; }
+    }
+
+    // Record staff identity verification.
+    // Reference from Microsoft.AspNet.Identity.Samples.
+    #region Record staff
+    public class SystemUser
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Account { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreateOn { get; set; }
+
+        public SystemUser()
+        {
+            this.Id = Guid.NewGuid();
+
+            this.SystemRoles = new List<SystemRole>();
+        }
+
+        public ICollection<SystemRole> SystemRoles { get; set; }
+    }
+
+    public class SystemRole
+    {
+        [Key]
+        [Required]
+        public Guid ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Sort { get; set; }
+
+        [Required]
+        public bool IsEnable { get; set; }
+
+        [Required]
+        public Guid CreateBy { get; set; }
+
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreateOn { get; set; }
+
+        public Guid? UpdateBy { get; set; }
+
+        public DateTime? UpdateOn { get; set; }
+
+        public SystemRole()
+        {
+            this.ID = Guid.NewGuid();
+            this.IsEnable = false;
+            this.CreateBy = new Guid();
+
+            this.SystemUsers = new List<SystemUser>();
+        }
+
+        public ICollection<SystemUser> SystemUsers { get; set; }
+
+    }
+    #endregion
+}
